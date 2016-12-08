e4321a27a9cc911307c35babd0585df2fc3893e2
fieldsfarmer <fieldsfarmer@gmail.com>
2016-12-07 22:53:57 -0500

    add createdAt and completedAd timestamp

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -16,17 +16,38 @@ describe('TodoApp', ()=>{
     todoApp.setState({todos:[]});
     todoApp.handleAddTodo(todoText);
     expect(todoApp.state.todos[0].text).toBe(todoText);
+
+    // expect createdAt to be a number
+    expect(todoApp.state.todos[0].createdAt).toBeA('number');
   });
   it('should toggle completed value while handleToggle called', ()=>{
     var todoData = {
       id: 1,
       text: 'sing a song',
-      completed: false
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
     };
     var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
     todoApp.setState({todos:[todoData]});
     expect(todoApp.state.todos[0].completed).toBe(false);
     todoApp.handleToggle(todoData.id);
     expect(todoApp.state.todos[0].completed).toBe(true);
+    // expect completedAt to be a number
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
+  });
+  it('should toggle todo from completed to incompleted', ()=>{
+    var todoData = {
+      id: 2,
+      text: 'something',
+      completed: true,
+      createdAt: 0,
+      completedAt: 1
+    };
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos:[todoData]});
+    todoApp.handleToggle(todoData.id);
+    // expect completedAt to be undefined
+    expect(todoApp.state.todos[0].completedAt).toNotExist();
   })
 });
---
+++ playground/moment-example.js
@@ -0,0 +1,10 @@
+var moment = require('moment');
+
+console.log(moment().format());
+var now = moment();
+console.log('Current timestamp', now.unix());
+
+var timestamp = 1481166935;
+var currentMoment = moment.unix(timestamp);
+console.log('Current moment', currentMoment.format('MMM D,YYYY @ h:mm a'));
+console.log('Current moment', currentMoment.format('MMMM Do,YYYY @ h:mm A'));
